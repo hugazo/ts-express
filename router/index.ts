5f51c4922bf42605c529d51eee66d4e19000d6f6
Hugo Morales <hugo@42devs.cl>
2020-12-23 23:19:58 -0300

    Updated types in router example

--- router/index.ts
+++ router/index.ts
@@ -1,8 +1,8 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (_req, res) => {
+router.get('/', (_req: Request, res: Response) => {
   try {
     res.status(200).send({
       message: 'Hello, from typescript express project',
